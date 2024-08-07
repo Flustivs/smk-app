33eec2c96c35807777c26191129c26002e8fc5b2
Flustivs <rune170206@gmail.com>
2024-08-07 09:33:21 +0200

    Fancy button made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { ThemePageComponent } from './theme-page/theme-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ThemePageComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+{path:'', component: HomePageComponent},
+{path:'home', component: HomePageComponent},
+{path:'theme', component: ThemePageComponent}
+    ]),
     AppRoutingModule
   ],
   providers: [
---
+++ src/app/theme-page/theme-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-page',
+  templateUrl: './theme-page.component.html',
+  styleUrl: './theme-page.component.css'
+})
+export class ThemePageComponent {
+
+}
