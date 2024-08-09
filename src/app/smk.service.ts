153cc748bfb0efa3c842055db8554a949f3686bf
Flustivs <rune170206@gmail.com>
2024-08-09 12:44:11 +0200

    Updated-getAllArt

--- src/app/smk.service.ts
+++ src/app/smk.service.ts
@@ -20,7 +20,7 @@ export class SmkService {
 
   async getAllArt(): Promise<any> {
     const url =
-      'https://api.smk.dk/api/v1/art/search/?keys=*&facetNum=-1&facets=creator,artist,colors,role_attributed_to,role_earlier_ascribed_to,role_workshop_of,role_follower,role_after,role_school,role_imitator_of,role_copy_after,role_after_model_by,role_publisher,role_printer,role_artist,creator_nationality,creator_gender,content_person,content_subject,object_names,techniques,materials,medium&randomHighlights=72041&lang=da&offset=0&rows=24';
+      'https://api.smk.dk/api/v1/art/search/?keys=*&facetNum=-1&facets=creator,artist,colors,role_attributed_to,role_earlier_ascribed_to,role_workshop_of,role_follower,role_after,role_school,role_imitator_of,role_copy_after,role_after_model_by,role_publisher,role_printer,role_artist,creator_nationality,creator_gender,content_person,content_subject,object_names,techniques,materials,medium&randomHighlights=72041&lang=da&offset=0&rows=2000';
     try {
       const response = await axios.get(url);
       return response.data;
