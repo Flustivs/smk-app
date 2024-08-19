b0e70db3803edbf334c92ed006f3065c02b4f9d3
Flustivs <rune170206@gmail.com>
2024-08-19 10:08:36 +0200

    Comments

--- src/app/smk.service.ts
+++ src/app/smk.service.ts
@@ -7,7 +7,10 @@ import axios from 'axios';
 export class SmkService {
   constructor() {}
 
-  // Calls the APi and awaits the response
+  /**
+   * Gets all the art from the api
+   * @returns API response.data
+   */
   async getAllArt(): Promise<any> {
     const url =
       'https://api.smk.dk/api/v1/art/search/?keys=*&facetNum=-1&facets=creator,artist,colors,role_attributed_to,role_earlier_ascribed_to,role_workshop_of,role_follower,role_after,role_school,role_imitator_of,role_copy_after,role_after_model_by,role_publisher,role_printer,role_artist,creator_nationality,creator_gender,content_person,content_subject,object_names,techniques,materials,medium&randomHighlights=72041&lang=da&offset=0&rows=2000';
@@ -20,7 +23,11 @@ export class SmkService {
     }
   }
 
-  // Call the API with a search key and awaits the response
+  /**
+   * Gets all the art with that specific key value
+   * @param key string
+   * @returns API response.data
+   */
   async getArtSearch(key: string): Promise<any> {
     const url = `https://api.smk.dk/api/v1/art/search?keys=${key}&facetNum=-1&facets=creator,artist,colors,role_attributed_to,role_earlier_ascribed_to,role_workshop_of,role_follower,role_after,role_school,role_imitator_of,role_copy_after,role_after_model_by,role_publisher,role_printer,role_artist,creator_nationality,creator_gender,content_person,content_subject,object_names,techniques,materials,medium&randomHighlights=72041&lang=da&offset=0&rows=2000`;
     try {
@@ -32,7 +39,15 @@ export class SmkService {
     }
   }
 
-  // Calls the API with a filter(s) and awaits the response
+  /**
+   * Gets all art with the choosen filter
+   * If filter = 'filters=' it just makes a normal fetch where it gets all art
+   *
+   * else it uses the filter to search
+   *
+   * @param filter string
+   * @returns API response.data
+   */
   async getFilterArt(filter: string): Promise<any> {
     let url = '';
     if (filter == 'filters=') {
